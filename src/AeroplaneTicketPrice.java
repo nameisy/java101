f0f8b9abf306444d8d6e1b710b52e43e40009ef7
İsmail Safa Yılmaz <nameisy@users.noreply.github.com>
2024-02-23 02:16:07 +0300

    Aeroplane Ticket Price Program.

---
+++ src/AeroplaneTicketPrice.java
@@ -0,0 +1,55 @@
+import  java.util.Scanner;
+
+//A programme that calculates the air ticket price according to the distance and conditions.
+public class AeroplaneTicketPrice {
+    public static void main(String[] args) {
+
+        // Defining variables.
+        int age, distance, tripType;
+        double price=0;
+
+        Scanner input= new Scanner(System.in);
+
+        //It asks the user for distance, age and journey type.
+        System.out.print("Enter the distance (in km): ");
+        distance= input.nextInt();
+
+        System.out.print("Enter your age: ");
+        age= input.nextInt();
+
+        System.out.print("Enter the trip type (1 => One Way, 2 => Round Trip): ");
+        tripType= input.nextInt();
+
+        // Calculation of the price according to the information provided.
+        if (distance>0 && age>0 && (tripType==1 || tripType==2)){
+            price= distance*0.10;
+            double discount, tipDiscount;
+
+            if (age<12) {
+                discount= price*0.5;
+                price=price-discount;
+            }
+            else if (age>12 && age<=24) {
+                discount= price*0.1;
+                price=price-discount;
+            }
+            else if (age>=65) {
+                discount= price*0.3;
+                price=price-discount;
+            }
+
+
+            if (tripType==2) {
+                tipDiscount= price*0.2;
+                price= (price-tipDiscount)*2 ;
+            }
+
+        }
+
+        else {
+            System.out.println("You Entered Incorrect Data!");
+        }
+
+        System.out.println(STR."Total Amount is \{price} TL");
+    }
+}
