ff0c0a1540f9f36c050dfd04e3887fbc8499f87c
İsmail Safa Yılmaz <nameisy@users.noreply.github.com>
2024-02-24 23:15:56 +0300

    Finding Forces Of 4 And 5.

---
+++ src/FindingForcesOf4And5.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class FindingForcesOf4And5 {
+    public static void main(String[] args) {
+
+        // Variable for user.
+        int number;
+
+        // Get the number from the user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = input.nextInt();
+
+        // Print the powers of 4
+        System.out.println("\nPowers of 4: ");
+        for (int i = 1; i <= n; i *= 4)
+            System.out.println(i + "");
+
+        // Print the powers of 4
+        System.out.println("\nPowers of 5: ");
+        for (int i = 1; i <= n; i *= 5)
+            System.out.println(i + "");
+
+    }
+}
