378684e2af37d78bfcc8cd3949fb608a80daef95
İsmail Safa Yılmaz <nameisy@users.noreply.github.com>
2024-02-26 01:46:06 +0300

    Perfect Number Check

---
+++ src/PerfectNumberCheck.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PerfectNumberCheck {
+
+    public static void main(String[] args) {
+        // Requests a number from the user.
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        int total = 0;
+
+        for (int i = 1; i < number; ++i) {
+            // Eğer sayi, i'ye tam bölünüyorsa, i toplama eklenir.
+            if (number % i == 0) {
+                total += i; }
+        }
+        if (total == number) {
+            // Print that it is a perfect number.
+            System.out.println(number + " Perfect number.");
+        } else {
+            // Prints that it is not a perfect number.
+            System.out.println(number + " Not a perfect number.");
+        }
+    }
+}
