de5acf1a4e2bd7517f6d7784e221102e9f81cd92
İsmail Safa Yılmaz <nameisy@users.noreply.github.com>
2024-02-25 20:21:34 +0300

    Armstrong Finding Numbers

--- src/ArmstrongFindingNumbers.java
+++ src/ArmstrongFindingNumbers.java
@@ -5,12 +5,13 @@ public class ArmstrongFindingNumbers {
         int total = 0;
 
         // While the number is greater than 0, the loop continues.
-        while (total > 0) {
+        while (a > 0) {
+
             // The remainder of the number when divided by 10 is added to the total.
             total += a % 10;
 
             // The number is divided by 10 to remove the last digit.
-            total /= 10;
+            a /= 10;
         }
         // The number is divided by 10 to remove the last digit.
         System.out.println("Total of the digits of the number: " + total);
